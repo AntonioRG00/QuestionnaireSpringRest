5cb8cb28ea4a631a08fd8d3eb40a147a4820e64f
= <=>
2021-04-11 01:24:49 +0200

    Se agrega Spring Security con configuración y plantillas

---
+++ src/main/java/es/antoniorg/myspringrest/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package es.antoniorg.myspringrest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// Si necesitamos simplemente un controlador que nos lleve a una plantilla lo
+		// añadimos desde aquí para no tener que crear un controlador
+		
+		registry.addViewController("/login");
+	}
+	
+	
+
+}
---
+++ src/main/java/es/antoniorg/myspringrest/controller/MainController.java
@@ -0,0 +1,13 @@
+package es.antoniorg.myspringrest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/inicio")
+	public String inicio() {
+		return "index";
+	}
+}
