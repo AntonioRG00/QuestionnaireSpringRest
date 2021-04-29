7a69ece3880bdd90f1b2a59f32a397c396e4ce61
= <=>
2021-04-29 22:27:01 +0200

    Se añade el login, el logout y más seguridad

--- src/main/java/es/antoniorg/myspringrest/security/SecurityConfig.java
+++ src/main/java/es/antoniorg/myspringrest/security/SecurityConfig.java
@@ -13,17 +13,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("admin")
-				.password("admin").roles("ADMIN");
+		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("admin").password("admin").roles("ADMIN");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/webjars/**", "/css/**","/javax.faces.resource/**", "**").permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+				.antMatchers("/webjars/**", "/css/**", "/javax.faces.resource/**", "/rest_idioma/**", "/rest_area/**",
+						"/rest_categoria/**", "/rest_pregunta/**", "/rest_respuesta/**", "/rest_preguntarespuesta/**")
+				.permitAll().anyRequest().authenticated();
 		http.formLogin().loginPage("/login.xhtml").permitAll().and().logout();
 		http.sessionManagement().maximumSessions(1);
-		
-		
+
+		// Parte del logout
+		http.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+
 		http.csrf().disable();
 	}
 }
