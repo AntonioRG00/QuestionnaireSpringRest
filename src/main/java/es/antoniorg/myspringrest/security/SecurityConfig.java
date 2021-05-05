96b6525222197269992de56a23eb54687e5896dd
= <=>
2021-05-05 18:53:12 +0200

    Se añade poder cambiar el acceso a urls sin login desde el properties

--- src/main/java/es/antoniorg/myspringrest/security/SecurityConfig.java
+++ src/main/java/es/antoniorg/myspringrest/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package es.antoniorg.myspringrest.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,22 +13,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Value("${urlsPermitidas}.split(',')")
+	private String[] urlsPermitidas;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		
 		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/webjars/**", "/css/**", "/javax.faces.resource/**", "/rest_idioma/**", "/rest_area/**",
-						"/rest_categoria/**", "/rest_pregunta/**", "/rest_respuesta/**", "/rest_preguntarespuesta/**", "/rest/**")
-				.permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(urlsPermitidas).permitAll().anyRequest().authenticated();
 		http.formLogin().loginPage("/login.xhtml").permitAll().and().logout();
 		http.sessionManagement().maximumSessions(3);
 
-		// Parte del logout
 		http.logout().logoutUrl("/logout").logoutSuccessUrl("/");
 
 		http.csrf().disable();
