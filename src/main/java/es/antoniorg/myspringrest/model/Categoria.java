456dd04f6d9742866d5e74e80321e1164f6373ce
Antonio Rodríguez Gundermann <antoniorg00@protonmail.com>
2021-06-13 23:26:56 +0200

    limpiado imports

--- src/main/java/es/antoniorg/myspringrest/model/Categoria.java
+++ src/main/java/es/antoniorg/myspringrest/model/Categoria.java
@@ -15,9 +15,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.ApiModelProperty;
